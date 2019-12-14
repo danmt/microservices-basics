e5cbc27b75f504e69f661bc7e5cd8153111f9fde
Daniel Marin <danielarturomt@gmail.com>
2019-12-14 13:10:29 -0300

    created the first microservice

---
+++ service-a/src/main.ts
@@ -0,0 +1,18 @@
+import { NestFactory } from '@nestjs/core';
+import { Transport } from '@nestjs/microservices';
+import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const logger = new Logger();
+
+async function bootstrap() {
+  const app = await NestFactory.createMicroservice(AppModule, {
+    transport: Transport.TCP,
+    options: {
+      host: '127.0.0.1',
+      port: 8888,
+    },
+  });
+  app.listen(() => logger.log('Microservice A is listening'));
+}
+bootstrap();
