5212faa292238306edfddaddf0d19468ad3f7755
Daniel Marin <danielarturomt@gmail.com>
2019-12-14 13:44:25 -0300

    created the api gateway

---
+++ api-gateway/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ClientsModule.register([
+      {
+        name: 'SERVICE_A',
+        transport: Transport.TCP,
+        options: {
+          host: '127.0.0.1',
+          port: 8888,
+        },
+      },
+    ]),
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
